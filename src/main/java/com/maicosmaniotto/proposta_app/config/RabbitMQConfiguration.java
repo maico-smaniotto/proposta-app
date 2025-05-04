8f71139fefc117705d9a68086e237dafca8721e5
maico-smaniotto <maicosmaniotto@gmail.com>
2025-05-03 23:28:38 -0300

    Configuração das filas do RabbitMQ

---
+++ src/main/java/com/maicosmaniotto/proposta_app/config/RabbitMQConfiguration.java
@@ -0,0 +1,71 @@
+package com.maicosmaniotto.proposta_app.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    private final ConnectionFactory connectionFactory;
+
+    public RabbitMQConfiguration(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    @Bean
+    public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
+        return QueueBuilder.durable("proposta-pendente.ms-analise-credito").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaPendenteMsNotificacao() {
+        return QueueBuilder.durable("proposta-pendente.ms-notificacao").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsProposta() {
+        return QueueBuilder.durable("proposta-concluida.ms-proposta").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsNotificacao() {
+        return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin() {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializarRabbitAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public FanoutExchange criarFanoutExchangePropostaPendente() {
+        return new FanoutExchange("proposta-pendente.ex");
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendenteMsAnaliseCredito() {
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsAnaliseCredito())
+            .to(criarFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendenteMsNotificacao() {
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsNotificacao())
+            .to(criarFanoutExchangePropostaPendente());
+    }
+
+}
